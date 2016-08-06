897273fc83c7fcf59a673656ec8a967f10d612a5
xiaofan <1353815633@qq.com>
2016-08-06 11:17:38 +0800

    根据json 数据创建控制器

--- XFWeibo-Swift/XFWeibo-Swift/Classes/Main/XFMainViewController.swift
+++ XFWeibo-Swift/XFWeibo-Swift/Classes/Main/XFMainViewController.swift
@@ -12,13 +12,55 @@ class XFMainViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(XFHomeViewController())
+        
         view.backgroundColor = UIColor.blueColor()
         
-        addChildViewController("XFHomeViewController", title: "首页", imageName: "tabbar_home")
-        addChildViewController("XFMsgViewController", title: "消息", imageName: "tabbar_message_center")
-        addChildViewController("XFDisViewController", title: "发现", imageName: "tabbar_discover")
-        addChildViewController("XFProfileViewController", title: "我", imageName: "tabbar_profile")
+        // 获取 json 文件路径
+        guard let jsonPath = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil) else {
+            print("没有文件路径")
+            return
+        }
+        
+        // 读取文件信息
+        guard let jsonData = NSData(contentsOfFile: jsonPath) else {
+            print("没有获取到数据")
+            return
+        }
+        
+        // 将二进制数据专程数组
+        guard let anyObject = try? NSJSONSerialization.JSONObjectWithData(jsonData, options: .MutableContainers) else {
+            
+            return
+        }
+        
+        guard let dicArray = anyObject as? [[String : AnyObject]] else {
+            
+            return
+        }
+        
+        // 遍历字典数组
+        for dict in dicArray {
+            // 获取控制器对应的字符串
+            guard let vcName = dict["vcName"] as? String else {
+                continue
+            }
+            
+            // 获取控制器显示的 title
+            guard let title = dict["title"] as? String else {
+                continue
+            }
+            
+            // 获取控制器显示的图标名字
+            guard let imageName = dict["imageName"] as? String else {
+                continue
+            }
+            
+            // 添加子控制器
+            addChildViewController(vcName, title: title, imageName: imageName)
+            
+        }
+        
+        
     }
     
     private func addChildViewController(childVCName: String, title : String, imageName : String) {
