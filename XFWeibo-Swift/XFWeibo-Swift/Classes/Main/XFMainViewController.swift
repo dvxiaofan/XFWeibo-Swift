0cbe36be93e3697736694f0ceacb18a6844e3420
xiaofan <1353815633@qq.com>
2016-08-06 12:54:18 +0800

    添加发布按钮

--- XFWeibo-Swift/XFWeibo-Swift/Classes/Main/XFMainViewController.swift
+++ XFWeibo-Swift/XFWeibo-Swift/Classes/Main/XFMainViewController.swift
@@ -10,18 +10,40 @@ import UIKit
 
 class XFMainViewController: UITabBarController {
     
+    // MARK:- 懒加载
+    private lazy var composeBtn : UIButton = UIButton()
+    
+    // MARK:- 系统回调方法
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //view.backgroundColor = UIColor.blueColor()
+        setupComposeBtn()
         
-        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
         
     }
     
 
 }
 
+// MARK: - 设置界面
+extension XFMainViewController {
+    /// 设置发布按钮
+    private func setupComposeBtn() {
+        tabBar.addSubview(composeBtn)
+        
+        composeBtn.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: .Normal)
+        composeBtn.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: .Highlighted)
+        composeBtn.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: .Normal)
+        composeBtn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: .Highlighted)
+        
+        composeBtn.sizeToFit()
+        
+        composeBtn.center = CGPointMake(tabBar.center.x, tabBar.bounds.size.height * 0.5)
+    }
+}
 
 
 
