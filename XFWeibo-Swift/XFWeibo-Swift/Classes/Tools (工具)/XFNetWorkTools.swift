f171f379fa3159661270975e919d2db00d2d4920
xiaofan <1353815633@qq.com>
2016-08-10 13:24:09 +0800

    封装 AFN 请求

---
+++ XFWeibo-Swift/XFWeibo-Swift/Classes/Tools (工具)/XFNetWorkTools.swift
@@ -0,0 +1,69 @@
+//
+//  XFNetWorkTools.swift
+//  
+//
+//  Created by xiaofans on 16/8/10.
+//  Copyright © 2016年 xiaofan. All rights reserved.
+// 外部直接调用请求方法request
+//
+
+import AFNetworking
+
+
+enum requestType : String {
+    case GET = "GET"
+    case POST = "POST"
+}
+
+class XFNetWorkTools: AFHTTPSessionManager {
+    // MARK:- 单例
+    // let 是线程安全的
+    static let shareInstance : XFNetWorkTools = {
+        let tools = XFNetWorkTools()
+        tools.responseSerializer.acceptableContentTypes?.insert("text/html")
+        
+        return tools
+    } ()
+    
+}
+
+// MARK:- 封装请求方法
+extension XFNetWorkTools {
+    func request(methodType methodType : requestType, urlString : String, parameters : [String : AnyObject], finished : (result : AnyObject?, error : NSError?) -> ()) {
+        
+        // 1.定义成功的回调闭包
+        let successCallBack = { (task : NSURLSessionDataTask, result : AnyObject?) -> Void in
+            finished(result: result, error: nil)
+        }
+        
+        // 2. 定义失败的回调闭包
+        let failureCallBack = { (task : NSURLSessionDataTask?, error : NSError) -> Void in
+            finished(result: nil, error: error)
+        }
+        
+        // 3. 发送网络请求
+        if methodType == .GET {
+            GET(urlString, parameters: parameters, progress: nil, success: successCallBack, failure: failureCallBack)
+        } else {
+            POST(urlString, parameters: parameters, progress: nil, success: successCallBack, failure: failureCallBack)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
